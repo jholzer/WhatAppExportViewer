e320d8b82c4a6d357df27553f3305ea169fb3b6f
jholzer <info@emvoll.de>
2020-02-12 14:07:41 +0100

    draft andorid app (just frame)

---
+++ WhatAppExportViewer/WhatsAppMobileExportViewer/WhatsAppMobileExportViewer/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+
+namespace WhatsAppMobileExportViewer
+{
+    public class ViewModelLocator
+    {
+        private readonly IKernel kernel;
+
+        public ViewModelLocator()
+        {
+            kernel = new StandardKernel();
+        }
+        public MainPageViewModel MainWindowViewModel => kernel.Get<MainPageViewModel>();
+    }
+}
---
+++ WhatAppExportViewer/WhatsAppMobileExportViewer/WhatsAppMobileExportViewer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsAppMobileExportViewer.ViewModels
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        public MainPageViewModel()
+        {
+            CmdSelectFile = ReactiveCommand.CreateFromTask(_ =>
+            {
+                return Task.FromResult(Unit.Default);
+            });
+        }
+
+        public ReactiveCommand<Unit, Unit> CmdSelectFile { get; }
+    }
+}
