d74db038d4e22385acbbdf54126b39bbfd9d5133
Jürgen Holzer <info@emvoll.de>
2020-02-12 23:58:35 +0100

    Main VM with some command scelletons

--- WhatsBack/WhatsBack/MainPage.xaml.cs
+++ WhatsBack/WhatsBack/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
 using ReactiveUI.XamForms;
 using Xamarin.Forms;
 
@@ -17,6 +19,27 @@ namespace WhatsBack
         public MainPage()
         {
             InitializeComponent();
+
+            ViewModel = new MainPageViewModel();
+
+            // Setup the bindings.  
+            // Note: We have to use WhenActivated here, since we need to dispose the  
+            // bindings on XAML-based platforms, or else the bindings leak memory.  
+            this.WhenActivated(disposable =>
+            {
+                //this.Bind(ViewModel, x => x.UserName, x => x.Username.Text)
+                //    .DisposeWith(disposable);
+
+                this.BindCommand(ViewModel, 
+                        x => x.CmdLoadFile, 
+                        x => x.LoadFile)
+                    .DisposeWith(disposable);
+
+                this.BindCommand(ViewModel, 
+                        x => x.CmdScanFolder, 
+                        x => x.ScanFolder)
+                    .DisposeWith(disposable);
+            });
         }
     }
 }
--- WhatsBack/WhatsBack/MainPageViewModel.cs
+++ WhatsBack/WhatsBack/MainPageViewModel.cs
@@ -1,11 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using ReactiveUI;
 
 namespace WhatsBack
 {
-    public class MainPageViewModel : ReactiveObject
+    public class MainPageViewModel : ViewModelBase
     {
+        public MainPageViewModel()
+        {
+            CmdLoadFile = ReactiveCommand.CreateFromTask(_ => LoadFile()).DisposeWith(Disposables);
+            CmdScanFolder = ReactiveCommand.CreateFromTask(_ => ScanFolder()).DisposeWith(Disposables);
+        }
+
+        public ReactiveCommand<Unit, Unit> CmdScanFolder { get; }
+
+        public ICommand CmdLoadFile { get; }
+
+        private Task ScanFolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task LoadFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ViewModelBase : ReactiveObject, IDisposable
+    {
+        protected CompositeDisposable Disposables = new CompositeDisposable();
+
+        public void Dispose()
+        {
+            Disposables?.Dispose();
+        }
     }
 }
